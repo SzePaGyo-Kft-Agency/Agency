c97e9299bf99bd2b17ea5a869268d77497d1cfa9
PIKSSL <purpledog120@gmail.com>
2023-01-04 12:14:20 +0100

    AgencyController

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Agency;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Client\Request;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function index(){
+        $agency =  Agency::all();
+        return $agency;
+    }
+    
+    public function show($id)
+    {
+        $agency = Agency::find($id);
+        return $agency;
+    }
+    public function destroy($id)
+    {
+        Agency::find($id)->delete();
+    }
+    public function store(Request $request)
+    {
+        $agency = new Agency();
+        $agency->name = $request->name;
+        $agency->country = $request->country;
+        $agency->type = $request->type;
+        $agency->save();
+    }
+
+    public function update(Request $request, $id)
+    {
+        $agency = Agency::find($id);
+        $agency->name = $request->name;
+        $agency->country = $request->country;
+        $agency->type = $request->type;
+    }
 }
