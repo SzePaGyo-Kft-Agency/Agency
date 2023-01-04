196f6e807bd7aace0fc1be86ba1a12041f166b97
Klittina <krisztike113@gmail.com>
2023-01-04 12:31:10 +0100

    users - Szedlár Krisztina

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,11 +17,19 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->integer('permission')->default(1);
+            $table->boolean('vip')->default(0);
             $table->timestamps();
         });
+
+        User::create(['name' => 'Alig Elek' , 'email'=> 'aelek@mail.com', 'password' => Hash::make('Aa123451@'),'vip'=>0]);
+        User::create(['name' => 'Winch Eszter' , 'email'=> 'weszter@mail.com', 'password' => Hash::make('Aa123452@'),'vip'=>0]);
+        User::create(['name' => 'Szerep Elek' , 'email'=> 'szepelek@mail.com', 'password' => Hash::make('Aa123453@'),'vip'=>0]);
+        User::create(['name' => 'Kis Károly' , 'email'=> 'karcsika@mail.com', 'password' => Hash::make('Aa123454@'),'vip'=>1]);
+        User::create(['name' => 'Nagy Barnabás' , 'email'=> 'barnika@mail.com', 'password' => Hash::make('Aa123455@'),'vip'=>0]);
+        User::create(['name' => 'Füty Imre' , 'email'=> 'imruska@mail.com', 'password' => Hash::make('Aa123456@'),'vip'=>1]);
+
     }
 
     /**
