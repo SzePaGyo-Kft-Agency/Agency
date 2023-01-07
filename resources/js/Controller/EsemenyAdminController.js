c99a8ea904971fe965dbbb3bf15cf05e7cf4952f
PIKSSL <purpledog120@gmail.com>
2023-01-07 11:17:30 +0100

    Controller basis

---
+++ resources/js/Controller/EsemenyAdminController.js
@@ -0,0 +1,11 @@
+
+class EsemenyAdminController{
+
+    constructor(){
+
+    }
+    
+
+}
+
+export default EsemenyAdminController;
---
+++ resources/js/Controller/UgynoksegAdminController.js
@@ -0,0 +1,11 @@
+
+class UgynoksegAdminController{
+
+    constructor(){
+
+    }
+    
+
+}
+
+export default UgynoksegAdminController;
